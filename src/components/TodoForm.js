13cfb987bc81a66cb80e2a3edfcebe0deefd9d97
cgregs32 <cgregs32@gmail.com>
2018-03-24 11:15:50 -0600

    test autofocus and form accepts text

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default props =>
   <form>
     <input
+      autoFocus
+      value={props.currentTodo}
+      onChange={props.handleNewTodoChange}
       type='text'
       className="new-todo"
       placeholder="What needs to be done?"/>
