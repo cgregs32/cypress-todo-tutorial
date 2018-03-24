e421f325dfc2e62d6a6a73dd12d51bdf9e200fc4
cgregs32 <cgregs32@gmail.com>
2018-03-24 12:58:33 -0600

    test for delete function of todo items

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 import Footer from './Footer';
-import { saveTodo, loadTodos } from '../lib/service';
+import { saveTodo, loadTodos, deleteTodo } from '../lib/service';
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -15,12 +15,13 @@ export default class TodoApp extends Component {
     };
     this.handleNewTodoChange = this.handleNewTodoChange.bind(this);
     this.handleTodoSubmit = this.handleTodoSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     loadTodos()
-      .then(({data}) => this.setState({todos: data}))
-      .catch(() => this.setState({error: true}))
+      .then(({ data }) => this.setState({ todos: data }))
+      .catch(() => this.setState({ error: true }));
   }
 
   handleNewTodoChange(e) {
@@ -30,18 +31,29 @@ export default class TodoApp extends Component {
   handleTodoSubmit(e) {
     e.preventDefault();
     const newTodo = { name: this.state.currentTodo, isComplete: false };
-    saveTodo(newTodo).then(({ data }) =>
-      this.setState({ todos: this.state.todos.concat(data), currentTodo: '' })
-    ).catch(() => this.setState({error: true}))
+    saveTodo(newTodo)
+      .then(({ data }) =>
+        this.setState({ todos: this.state.todos.concat(data), currentTodo: '' })
+      )
+      .catch(() => this.setState({ error: true }));
+  }
+
+  handleDelete(id) {
+    deleteTodo(id).then(() =>
+      this.setState({
+        todos: this.state.todos.filter(t => t.id !== id)
+      })
+    );
   }
 
   render() {
+    const remaining = this.state.todos.filter(t => !t.isComplete).length;
     return (
       <Router>
         <div>
           <header className="header">
             <h1>todos</h1>
-            {this.state.error && <span className='error'>Oh No</span>}
+            {this.state.error ? <span className="error">Oh No</span> : null}
             <TodoForm
               currentTodo={this.state.currentTodo}
               handleNewTodoChange={this.handleNewTodoChange}
@@ -49,9 +61,12 @@ export default class TodoApp extends Component {
             />
           </header>
           <section className="main">
-            <TodoList todos={this.state.todos} />
+            <TodoList
+              handleDelete={this.handleDelete}
+              todos={this.state.todos}
+            />
           </section>
-          <Footer />
+          <Footer remaining={remaining} />
         </div>
       </Router>
     );
